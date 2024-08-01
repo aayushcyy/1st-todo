7da5e777978c4702e00f0556e786be641ce26886
Aayush Chaudhary <105271410+aayushcyy@users.noreply.github.com>
2024-08-01 23:06:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+// Accessing Value from the Input
+function taskAdder() {
+  let taskInput = document.querySelector("#new-task");
+
+  taskInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      // Correct condition to check for the "Enter" key
+      let taskText = taskInput.value; // Get the task text within the event listener
+      if (taskText) {
+        document.querySelector("#main").innerHTML += `
+      <div class="task">
+          <div class="task-p1">
+            <input type="checkbox" class="task-check" />
+            <p>${taskText}</p>
+          </div>
+          <button id="delete-btn">
+            <i class="ri-delete-bin-6-line"></i>
+          </button>
+        </div>
+      `;
+        taskInput.value = ""; // Reset the input field
+      }
+    }
+  });
+}
+// task delegation
+function taskDeleter() {
+  document.querySelector("#main").addEventListener("click", function (event) {
+    if (event.target.closest("#delete-btn")) {
+      let currElement = event.target.closest(".task");
+      currElement.remove();
+    }
+  });
+}
+taskAdder();
+taskDeleter();
+
+let themeBtn = document.querySelector("#theme-btn");
+let logoText = document.querySelector("#logo");
+let inputIcon = document.querySelector("#plus-icon");
+let inpuText = document.querySelector("#new-task");
+
+let btnState = 0;
+
+themeBtn.addEventListener("click", function () {
+  if (btnState === 0) {
+    themeBtn.style.color = "#788cde";
+    logoText.style.color = "#788cde";
+    inputIcon.style.color = "#788cde";
+    inpuText.style.color = "#788cde";
+    document.documentElement.style.setProperty(
+      "--placeholder-color",
+      "#788cde"
+    );
+    btnState++;
+  } else if (btnState === 1) {
+    themeBtn.style.color = "#ecbda2";
+    logoText.style.color = "#ecbda2";
+    inputIcon.style.color = "#ecbda2";
+    inpuText.style.color = "#ecbda2";
+    document.documentElement.style.setProperty(
+      "--placeholder-color",
+      "#ecbda2"
+    );
+    btnState++;
+  } else if (btnState === 2) {
+    themeBtn.style.color = "#e46c8c";
+    logoText.style.color = "#e46c8c";
+    inputIcon.style.color = "#e46c8c";
+    inpuText.style.color = "#e46c8c";
+    document.documentElement.style.setProperty(
+      "--placeholder-color",
+      "#e46c8c"
+    );
+    btnState++;
+  } else if (btnState === 3) {
+    themeBtn.style.color = "#479e98";
+    logoText.style.color = "#479e98";
+    inputIcon.style.color = "#479e98";
+    inpuText.style.color = "#479e98";
+    document.documentElement.style.setProperty(
+      "--placeholder-color",
+      "#479e98"
+    );
+    btnState = 0;
+  }
+});
